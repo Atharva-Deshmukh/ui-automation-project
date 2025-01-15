e9536b9f2f03d1278a18c5b0c5104783b5a14a3f
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-01-15 23:01:03 +0530

    go

---
+++ cypress/CYPRESS_NOTES/Commands/web_navigationRelated/go.ts
@@ -0,0 +1,17 @@
+/* Syntax
+cy.go(direction) Navigate back or forward to the previous or next URL in the browser's history.
+cy.go(direction, options)
+
+Direction
+Go back in browser's history
+cy.go('back') // equivalent to clicking back button
+
+Go forward in browser's history
+cy.go('forward') // equivalent to clicking forward button
+
+Number
+Go back in browser's history
+cy.go(-1) // equivalent to clicking back button
+
+Go forward in browser's history
+cy.go(1) // equivalent to clicking forward button */
