987962354fffe8a834dd4710ca3a9057ec823b58
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-01-27 20:50:12 +0530

    hooks in cypress

---
+++ cypress/CYPRESS_NOTES/hooks.ts
@@ -0,0 +1,19 @@
+/*
+before(() => {
+  // root-level hook
+  // runs once before all tests
+})
+
+beforeEach(() => {
+  // root-level hook
+  // runs before every test block
+})
+
+afterEach(() => {
+  // runs after each test block
+})
+
+after(() => {
+  // runs once all tests are done
+})
+*/
