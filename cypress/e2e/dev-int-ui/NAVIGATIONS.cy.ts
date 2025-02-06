fc7ed2ac15b079a84c5aab5d4226f26d8f0f5b23
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-02-06 23:17:11 +0530

    navigations

--- cypress/e2e/dev-int-ui/NAVIGATIONS.cy.ts
+++ cypress/e2e/dev-int-ui/NAVIGATIONS.cy.ts
@@ -161,7 +161,7 @@ describe('WORKFLOWS OF VARIOUS NAVIGATION FUNCTIONS', () => {
         
     });
 
-    it.only('childern()', () => {
+    it.skip('childern()', () => {
         /*  Get all following siblings of each DOM element in a set of matched DOM 
              elements up to, but not including, the element provided.
 
@@ -211,6 +211,113 @@ describe('WORKFLOWS OF VARIOUS NAVIGATION FUNCTIONS', () => {
         .first()
         .should('have.text', 'Logo Design');
         //returns [<li class="services-2">Logo Design</li>]
+        
+    });
+
+    it.skip('parent() and parents() and parentsUntil()', () => {
+        /*  .parent() only travels a single level up the DOM tree as opposed to the .parents() command.
+
+        DONT PASS SELECTOR in parent(), it misbehaves
+
+        parentsUntil() is always passed with a selector
+        
+        DOM USED:
+
+              <ul class="secondary-nav">
+                <li class="services-1">Web Design</li>
+                <li class="services-2">Logo Design</li>
+                <li class="services-3">
+                  Print Design
+                  <ul class="tertiary-nav">
+                    <li>Signage</li>
+                    <li>T-Shirt</li>
+                    <li>Business Cards</li>
+                  </ul>
+                </li>
+              </ul>
+*/
+
+        cy.visit('http://127.0.0.1:5500/DOMs/NavigationDOM.html', {timeout: uiTimeout});
+
+        // without selector
+        cy.get('li:contains("Signage")', {timeout: uiTimeout})
+        .should('be.visible')
+        .parent()
+        .should('have.class', 'tertiary-nav');
+        
+        // with selector - INCORRECT CODE
+        // cy.get('li:contains("T-Shirt")', {timeout: uiTimeout})
+        // .should('be.visible')
+        // .parent('.tertiary-nav')
+        // .should('be.visible')
+        // .and('have.class', '.tertiary-nav');
+
+        /* REASON:
+        .parent('.tertiary-nav') is incorrect because .parent() selects the direct parent, 
+        but <li>T-Shirt</li>'s direct parent is <ul class="tertiary-nav">, not .tertiary-nav itself.
+        
+        */
+
+        // without selector
+        cy.get('li:contains("Signage")', {timeout: uiTimeout})
+        .should('be.visible')
+        .parents()
+        .should('be.visible')
+        .and('have.length', 8);  // actual DOM is bigger than the one shown in comments
+
+        // with selector
+        cy.get('li:contains("Signage")', {timeout: uiTimeout})
+        .should('be.visible')
+        .parents('.secondary-nav')
+        .should('be.visible')
+        .and('have.length', 1);  
+
+        // parentsUntil() - always with selector
+        // Get all ancestors of each DOM element in a set of matched DOM elements up to, but not including, 
+        // the element provided.
+
+        // Misbehaves: keeps finding elements to the <html> node, hence use closest
+
+        // cy.get('li:contains("Signage")', {timeout: uiTimeout})
+        // .should('be.visible')
+        // .parentsUntil('.services-3')
+        // .should('be.visible')
+        // .then(($ele) => {
+        //   console.warn('ELE -> ', $ele)
+        // })
+        // .and('have.length', 2);    
+
+    });
+
+    it.only('closest()', () => {
+        /*  Get the first DOM element that matches the selector (whether it be itself or one of its ancestors).
+        
+            Always passed with selector
+
+        DOM USED:
+
+              <ul class="secondary-nav">
+                <li class="services-1">Web Design</li>
+                <li class="services-2">Logo Design</li>
+                <li class="services-3">
+                  Print Design
+                  <ul class="tertiary-nav">
+                    <li>Signage</li>
+                    <li>T-Shirt</li>
+                    <li>Business Cards</li>
+                  </ul>
+                </li>
+              </ul>
+*/
+
+        cy.visit('http://127.0.0.1:5500/DOMs/NavigationDOM.html', {timeout: uiTimeout});
+
+        // with selector
+        cy.get('li:contains("Signage")', {timeout: uiTimeout})
+        .should('be.visible')
+        .closest('ul.secondary-nav')
+        .should('be.visible')
+        .and('have.length', 1);
         
     });
 });
