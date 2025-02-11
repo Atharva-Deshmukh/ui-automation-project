47c7b9761e133bbd9525401df9b27d287d76c536
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-02-11 22:36:49 +0530

    custom queries

--- cypress/support/customQueries.ts
+++ cypress/support/customQueries.ts
@@ -4,7 +4,15 @@ Cypress.Commands.addQuery('getById', function (id: string) {
     return () => {
       console.log('getById called with:', id);
   
-      // Use Cypress.$() to get the element, ensuring it works within Cypress' retry mechanism
+      /* Use Cypress.$() to get the element, ensuring it works within Cypress' retry mechanism
+      Uses Cypress' jQuery Wrapper (Cypress.$()) Instead of document.getElementById()
+
+      Cypress.$() is aware of Cypress' automatic retries, while document.getElementById() is not.
+      This ensures Cypress waits for the element without breaking its execution model.
+
+       When the element is not found, we return an empty jQuery object (Cypress.$()), 
+       which triggers Cypress' built-in retry logic. */
+       
       const $element = Cypress.$(`#${id}`);
       console.warn('ELEMENT BY ID -> ', $element.length ? $element[0] : null);
   
