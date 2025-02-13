5d87b5fb03def57cc4a36ad3f0177b3433b9c8f4
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-02-13 22:33:59 +0530

    context menu

---
+++ cypress/e2e/UI_Automation/contextMenu.cy.ts
@@ -0,0 +1,38 @@
+import { should } from "chai";
+import { uiTimeout } from "../../fixtures/commonData"
+
+
+it('ContextMenu Opens on right click', () => {
+    cy.visit('https://www.softwaretestingmentor.com/automation-practice-page-right-click-demo/', {timeout: uiTimeout});
+
+    cy.contains('Right Click to see the menu', {timeout: uiTimeout})
+    .should('be.visible')
+    .rightclick()
+    .then(() => {
+        cy.scrollTo('top')
+        cy.get('.menuItems', {timeout: uiTimeout})
+        .should('be.visible')
+        .then(($ele) => {
+            cy.wrap($ele).find('li a[rel="noopener"]').each(($menu, index) => {
+                cy.log(`MENU ${index + 1} ->  `+ $menu.text());
+            });
+        });
+    });
+});
+
+it('ContextMenu Opens on hovering', () => {
+    cy.visit('https://swisnl.github.io/jQuery-contextMenu/demo/trigger-hover-autohide.html');
+
+    cy.contains('hover over me', {timeout: uiTimeout})
+    .should('be.visible')
+    .realHover()
+    .then(() => {
+        cy.get('.context-menu-list.context-menu-root', {timeout: uiTimeout})
+        .should('be.visible')
+        .then(($ele) => {
+            cy.wrap($ele).find('li span').each(($menu, index) => {
+                cy.log(`MENU ${index + 1} ->  `+ $menu.text());
+            });
+        });
+    });
+});
