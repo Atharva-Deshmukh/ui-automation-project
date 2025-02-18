cef29d1c70074fa33bb44d0acd25fbff80c5b6bc
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-02-18 22:47:17 +0530

    loggs removed

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -26,9 +26,11 @@ import "cypress-real-events";
 // });
 
 const app = window.top;
-if (!app.document.head.querySelector("[data-hide-command-log-request]")) {
- const style = app.document.createElement("style");
- style.innerHTML =".command-name-request, .command-name-xhr { display: none }";
- style.setAttribute("data-hide-command-log-request", "");
- app.document.head.appendChild(style); 
+if (!Cypress.env('SHOW_REQUEST_LOGS')) {  // Only hide logs when SHOW_REQUEST_LOGS is false or undefined
+  if (!app.document.head.querySelector("[data-hide-command-log-request]")) {
+    const style = app.document.createElement("style");
+    style.innerHTML = ".command-name-request, .command-name-xhr { display: none }";
+    style.setAttribute("data-hide-command-log-request", "");
+    app.document.head.appendChild(style);
+  }
 }
