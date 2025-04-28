4351e443a3a79ff83656e99ef7115519c94801b7
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-04-28 20:44:59 +0530

    clipboard handling cypress

--- cypress/e2e/UI_Automation/clipboardHandling.cy.ts
+++ cypress/e2e/UI_Automation/clipboardHandling.cy.ts
@@ -1,35 +1,16 @@
 import { uiTimeout } from "../../fixtures/commonData";
 
-let text: string = 'Hi I am Atharva';
-
-// we use cy.window()
 it('Copy text to clipboard and then verify it', () => {
-    cy.visit('https://www.w3schools.com/howto/howto_js_copy_clipboard.asp', {timeout: uiTimeout});
-    cy.get('#myInput', {timeout: uiTimeout})
-    .should('be.visible')
-    .clear()
-    .type(text);
-
-    cy.get('.tooltip > .w3-button', {timeout: uiTimeout})
-    .should('be.visible')
-    .click('center')
-    .then(($btn) => {
-
-        // To reveal the tooltip to indicate if the text is copied
-        cy.wrap($btn).realHover();
-
-
-        cy.wait(2000);
-        // verify that popup and check what is copied
-        cy.get('span.tooltiptext', {timeout: uiTimeout})
-        .should('be.visible')
-        .and('have.text', 'Copied: ' + text);
-
-        // access clipboard and verify it
-        cy.window().then((win) => {
-            win.navigator.clipboard.readText().then((textRead) => {
-                expect(textRead).to.eq(text);
-            })
+    cy.visit('https://clipboardjs.com/', {timeout: uiTimeout});
+    cy.get('p:contains("You can get it on npm.") + .snippet .btn').realHover().then(($btn) => {
+        cy.wrap($btn).should('be.visible')
+        .click()
+        .then(() => {
+            cy.window().then((win) => {
+                win.navigator.clipboard.readText().then((text) => {
+                    expect(text).to.eq('npm install clipboard --save');
+                });
+            });
         });
     });
 }); 
