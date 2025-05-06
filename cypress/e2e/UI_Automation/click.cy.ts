c96049f9f5d14361a414fc753eb995038c574933
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-05-06 22:18:56 +0530

    clicks

--- cypress/e2e/UI_Automation/click.cy.ts
+++ cypress/e2e/UI_Automation/click.cy.ts
@@ -13,8 +13,9 @@ SYNTAX: (same as dblclick, rightclick)
 Specify a corner of the element to click
 
 cy.get('img').click('topRight')
-The center position is the default position. Valid positions are topLeft, 
-top, topRight, left, center, right, bottomLeft, bottom, and bottomRight.
+The center position is the default position. 
+Valid positions:
+topLeft, top, topRight, left, center, right, bottomLeft, bottom, and bottomRight.
 
 Coordinates: ORIGIN: top left corner
 The click below will be issued inside of the element 
@@ -26,13 +27,14 @@ Force a click with relative coordinates
 
 
 Click all elements with id starting with 'btn'
-By default, Cypress will error if you're trying to click multiple elements. 
+By default, Cypress will give error if you're trying to click multiple elements. 
 By passing { multiple: true } Cypress will iteratively apply the click to each 
 element and will also log to the Command Log multiple times.
 
 cy.get('[id^=btn]').click({ multiple: true })
 
-                                    KEY COMBINATION
+                                    KEY COMBINATIONS
+                                    ---------------
 
 // execute a SHIFT + click on the first <li>
     cy.get('li:first').click({
@@ -44,6 +46,8 @@ cy.get('[id^=btn]').click({ multiple: true })
     altKey: true,
     })
 
+In Windows, the Meta key is referred to as the Windows key
+
 // execute a CMD + right click on the .menu-item
     cy.get('.menu-item').rightclick({
     metaKey: true,
@@ -54,6 +58,7 @@ cy.get('[id^=btn]').click({ multiple: true })
 import { uiTimeout } from "../../fixtures/commonData";
 
 describe('WORKFLOWS', () => {
+        /* Holding a key */
         it('LONG PRESS BUTTON and release', () => {
             cy.visit('https://practice.expandtesting.com/radio-buttons', { timeout: uiTimeout }).then((winObj) => {
                 expect(winObj.navigator.language).to.eq('en-US');
