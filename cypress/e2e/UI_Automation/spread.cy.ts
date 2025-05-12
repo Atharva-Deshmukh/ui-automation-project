78ac546128b20f9b511b37a66071da3507d4a034
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-05-12 22:13:32 +0530

    spread

--- cypress/e2e/UI_Automation/spread.cy.ts
+++ cypress/e2e/UI_Automation/spread.cy.ts
@@ -4,9 +4,11 @@ Identical to .then(), but always expects an array-like structure as its subject.
 .spread() yields the return value of your callback function.
 
 Ex: 
+
 cy.intercept('/users/*').as('getUsers')
 cy.intercept('/activities/*').as('getActivities')
 cy.intercept('/comments/*').as('getComments')
+
 cy.wait(['@getUsers', '@getActivities', '@getComments']).spread(
   (getUsers, getActivities, getComments) => {
     // each interception is now an individual argument
@@ -17,7 +19,12 @@ cy.wait(['@getUsers', '@getActivities', '@getComments']).spread(
 describe('Spread Workflow', () => {   
     it('Spread Workflow', () => {
         cy.wrap([2,3,6]).spread((arg1, arg2, arg3) => {
-            cy.log(arg1 + ' ' + arg2 + ' ' + arg3);
+          expect(arg1 + arg2 + arg3).to.eq(11);
+        });
+    });
+    it('Spread Workflow', () => {
+        cy.wrap([2,3,6]).spread((arg1, arg2) => {
+            expect(arg1 + arg2).to.eq(5);
         });
     });
 });
