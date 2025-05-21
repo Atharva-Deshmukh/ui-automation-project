fbbefa03e3e4be0598ed295d3690a30bb5d3d4fe
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-05-21 20:24:30 +0530

    sliders

--- cypress/e2e/UI_Automation/sliders.cy.ts
+++ cypress/e2e/UI_Automation/sliders.cy.ts
@@ -6,6 +6,7 @@ describe('Handling slider inputs', () => {
     it('Range based sliders', () => {
         cy.visit('http://127.0.0.1:5500/DOMs/SLIDERS.html', {timeout: uiTimeout});
         // we need to change val and then trigger change event
+        // val means we are invoking value()
         cy.get('#volume').invoke('val', 80).trigger('change'); 
         cy.get('p#response').should('include.text', 80);
 
@@ -23,9 +24,8 @@ describe('Handling slider inputs', () => {
         
            Approach for image based URLs, URLS change, so we can verify which image is in the slide
            Also, some DOM is hidden, we can click({force: true}) to change the slider
-           Also, we can direclty use buttons above to change the slide
-
-           Go to this URL directly */
+           Also, we can direclty use '<' '>' to change the slide
+           Also, we can verify the URL in case it changes  */
 
         cy.visit('https://qaboxletstestcypresspracticesite.netlify.app/styledslider#slide-1', {timeout: uiTimeout});
         cy.get('.slider a[href="#slide-3"]').click(); 
@@ -33,7 +33,6 @@ describe('Handling slider inputs', () => {
     });
 
     it('Material UI Slider', () => {
-
         /* DOM: This is a complex DOM
 
         <div class="MuiStack-root css-10sddac"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-q7mezt" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VolumeDownIcon">
@@ -44,8 +43,7 @@ describe('Handling slider inputs', () => {
         <span data-index="0" class="MuiSlider-thumb MuiSlider-thumbSizeMedium MuiSlider-thumbColorPrimary MuiSlider-thumb MuiSlider-thumbSizeMedium MuiSlider-thumbColorPrimary css-yq1go2" style="left: 45%;">
         <input data-index="0" aria-label="Volume" aria-valuenow="45" aria-orientation="horizontal" aria-valuemax="100" aria-valuemin="0" type="range" min="0" max="100" step="1" style="border:0;clip:rect(0 0 0 0);height:100%;margin:-1px;overflow:hidden;padding:0;position:absolute;white-space:nowrap;width:100%;direction:ltr" value="45"></span></span><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-q7mezt" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VolumeUpIcon"><path d="M3 9v6h4l5 5V4L7 9zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02M14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77"></path></svg></div>
         
-
-        Inspect the element and change every attribute that is changing
+        Inspect the element and CHANGE EVERY ATTRIBUTE THAT IS CHANGING.
 
         LOCATORS NEEDS TO BE OPTIMISED WITH XPATH */
 
@@ -58,7 +56,7 @@ describe('Handling slider inputs', () => {
 
     it('Different slider', () => {
 
-        /* DOM:
+        /* DOM: here progress is denoted just by using styles, so invoke and change styles
         
         <div id="slider-range" class="ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content">
             <div class="ui-slider-range ui-corner-all ui-widget-header" style="left: 30.4%; width: 42.8%;"></div>
@@ -66,18 +64,17 @@ describe('Handling slider inputs', () => {
             <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default" style="left: 73.2%;"></span>
         </div>
 
-        Use invoke('attr', 'style', 'left: 73.2%;')  // pick everything between "" for style value
-        
-        */
+        Use invoke('attr', 'style', 'left: 73.2%;')  // pick everything between "" for style value */
 
         cy.visit('https://testautomationpractice.blogspot.com/', {timeout: uiTimeout});
-        cy.get('.ui-slider-range.ui-corner-all.ui-widget-header').invoke('attr', 'style', 'left: 14%; width: 84%;').then(() => {
+        cy.get('.ui-slider-range.ui-corner-all.ui-widget-header').invoke('attr', 'style', 'left: 14%; width: 84%;')
+        .then(() => {
             cy.get('#slider-range span[tabindex="0"]:eq(0)').scrollIntoView().invoke('attr', 'style', 'left: 14%;'); 
             // corresponds to 490$
             cy.get('#slider-range span[tabindex="0"]:eq(1)').invoke('attr', 'style', 'left: 98%;'); 
             cy.get('input#amount').invoke('val').then((val) => {
                 expect(val).to.include('$75 - $300')
-            })
+            });
         }); 
     });
 
