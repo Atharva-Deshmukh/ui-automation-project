2a2752b67ba7b1a2eb117c4c776ce7656d2cfb34
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-05-24 18:14:31 +0530

    clock and tick

--- cypress/e2e/UI_Automation/StubAndIntercept/clock.cy.ts
+++ cypress/e2e/UI_Automation/StubAndIntercept/clock.cy.ts
@@ -1,52 +1,27 @@
-/* 
-cy.clock() overrides native global functions related to time allowing them to be controlled 
-synchronously via cy.tick() or the yielded clock object. This includes controlling:
-    setTimeout
-    clearTimeout
-    setInterval
-    clearInterval
-    Date Objects
+/* cy.clock() overrides native global functions related to time like
+    setTimeout, clearTimeout, setInterval, clearInterval, Date Objects
 
-The clock starts at the unix epoch (timestamp of 0). This means that when you instantiate new Date 
-in your application, it will have a time of January 1st, 1970.
+The clock starts at the unix epoch (timestamp of 0) and date = January 1st, 1970.
 
-                                                    YEILD
-cy.clock() yields a clock object with the following methods:
+                                                    YEILDs
+                                                    ------
 
-clock.tick(milliseconds)
-Move the clock the specified number of milliseconds. Any timers within the affected 
-range of time will be called.
-
-clock.restore()
-Restore all overridden native functions. This is automatically called between tests,
- so should not generally be needed.
-
-clock.setSystemTime(now)
-Change the system time to the new now. Now can be a timestamp, date object, or not passed in which 
-defaults to 0. No timers will be called, nor will the time left before they trigger change.
-
+                    Either do clock = cy.clock()
+                                   OR
 You can also access the clock object via this.clock in a .then() callback.
+cy.clock().then((clock) => {  // yields a clock object with following methods
 
+    clock.tick(milliseconds)
+    Move the clock the specified number of milliseconds. 
 
-                                        TRIGGERING a setInterval
-// your app code
-let seconds = 0
+    clock.restore()
+    Restore all overridden native functions. This is automatically called between tests,
+    so should not generally be needed.
 
-setInterval(() => {
-  $('#seconds-elapsed').text(++seconds + ' seconds')
-}, 1000)
-
-cy.clock()
-cy.visit('/index.html')
-cy.tick(1000)
-cy.get('#seconds-elapsed').should('have.text', '1 seconds')
-cy.tick(1000)
-cy.get('#seconds-elapsed').should('have.text', '2 seconds')
-
-
-
-
-*/
+    clock.setSystemTime(now)
+    Change the system time to the new now. Now can be a timestamp, date object, or not passed in which 
+    defaults to 0. No timers will be called, nor will the time left before they trigger change.
+})*/
 
 import { uiTimeout } from "../../../fixtures/commonData";
 
@@ -62,7 +37,8 @@ describe('WORKFLOWS', () => {
 
         cy.wrap(changeVal(obj));
 
-        cy.wrap(obj).should('have.own.property', 'key', 'VAL_UPDATED');  // test did run for 11 seconds literaly
+        cy.wrap(obj).should('have.own.property', 'key', 'VAL_UPDATED');  
+        // test did run for 11 seconds literaly
     });
 
     /* DOM used:
@@ -82,7 +58,7 @@ describe('WORKFLOWS', () => {
         forced the button to change in 229 ms only. VERY USEFUL FOR SLOW UI ELEMENTS
         */
 
-    it('With clock, we can modify the systems time and finish the spec in less time', () => {
+    it('Make the test Faster: With clock, we can modify the systems time and finish the spec in less time', () => {
         cy.clock(); // Freeze the clock
 
         cy.visit('http://127.0.0.1:5500/DOMs/CLOCK.html', {timeout: uiTimeout});
@@ -94,10 +70,40 @@ describe('WORKFLOWS', () => {
 
         // Fast-foreward time
         cy.tick(11000).then(() => {
-            cy.get('#VAL', {timeout: uiTimeout}).should('have.text', 'Click Me UPDATED'); // Assertion should now pass immediately
+            cy.get('#VAL', {timeout: uiTimeout}).should('have.text', 'Click Me UPDATED'); 
+            // Assertion should now pass immediately
         })
     
         cy.clock().invoke('restore'); // Restore the system clock
+    });
+
+    it('For native JS, use window obj', () => {
+        let count = 0;
+
+        cy.clock();
+
+        cy.window().then((win) => {
+            win.setInterval(() => {
+                count++;
+            }, 1000);
+        });
+
+        cy.tick(5000).then(() => {
+            expect(count).to.equal(5);
+        });
+
+        // restore the clock
+        cy.clock().then((clock) => {
+            clock.restore();
+        })
+    });
+
+    it.only('Specify a now timestamp', () => {
+        const now = new Date(2021, 3, 14) // month is 0-indexed
+
+        cy.clock(now)
+        cy.visit('/')
+        cy.get('#date').should('have.value', '04/14/2021')
     });
     
 });
