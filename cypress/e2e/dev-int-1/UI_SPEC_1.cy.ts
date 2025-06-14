348d5c55bbe7c22e527d46ab64a3adeb73264f3f
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-06-14 14:17:43 +0530

    feat: scripts in cypress

---
+++ cypress/e2e/dev-int-1/UI_SPEC_1.cy.ts
@@ -0,0 +1,7 @@
+it('TC - 1', () => {
+  expect(true).to.be.true;
+});
+
+it('TC - 2', () => {
+  expect(false).to.be.false;
+});
---
+++ cypress/e2e/dev-int-2/UI_SPEC_2.cy.ts
@@ -0,0 +1,7 @@
+it('TC - 1', () => {
+  expect(undefined).to.be.undefined;
+});
+
+it('TC - 2', () => {
+  expect(null).to.be.null;
+});
